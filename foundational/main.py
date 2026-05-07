bedc1792d08b4a1a19d50c3360555ff6503e0ea3
Nitesh Jawanjal <47380565+niteshaj@users.noreply.github.com>
2026-05-07 13:17:06 +0000

    added:   foundational/main.py

--- foundational/main.py
+++ foundational/main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from foundational!")
+import os
+from urllib import response
+from langchain_google_genai import ChatGoogleGenerativeAI
 
+
+def main():
+    API_KEY = os.environ.get('GOOGLE_API_KEY')
+    model = ChatGoogleGenerativeAI(model='gemini-2.5-flash-lite', api_key=API_KEY)
+    response = model.invoke("What's the capital of the Moon?")
+    print(response.content)
 
 if __name__ == "__main__":
     main()
